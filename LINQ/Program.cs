b389c6631e97140fad003d9000e139cc3b8ddfd4
Robert Woods <robert.h.woods@hotmail.com>
2020-05-02 14:09:32 -0400

    Add project files.

---
+++ LINQ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+
+
+            //LINQ Extension Methods
+            var book = books.First(b => b.Title == "C# Advanced Topics");
+
+            Console.WriteLine(book.Title + " " + book.Price);
+            //Console.WriteLine(book == null);
+
+        }
+    }
+}
